12a0823522c4d4c59f3552e3f6305969bf05edae
Gabby <gabbympinto@gmail.com>
2023-02-28 14:10:13 -0500

    added weather api link

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+ 
+
+ function getWeather() {
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={1a0e2919a9b4dd8457669d885e90b7b9}';
+    fetch(weatherUrl)
+        .then(function {
+
+
+        });
+
+
+
+ };
