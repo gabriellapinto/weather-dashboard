331a0c7d1135d360548c59ada9faf8d0657d863f
Gabby <gabbympinto@gmail.com>
2023-03-14 20:40:38 -0400

    added if statement and tweaked other code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,51 @@
+ var searchBtn = $("#search-btn");
+ var searchFormEl = $("#search-form");
+ var cityHistory = JSON.parse(localStorage.getItem('city')) ?? [];
  
  
  
  
  
  
+
+
+
  
  // function for local storage
  function weatherInput() {
-    var searchBtn = $("#search-btn");
-    var userInput = $("city-search-form".val());
+    var userInput = $("#search-form").val();
 
     localStorage.setItem("userInput", userInput);
 
+
  }
- // function for click event for search button
+
+ // function for click event for the search button
  function searchFormSubmit(event) {
+    // prevents page from reloading when button is clicked
     event.preventDefault();
 
+    var searchInputVal = $("#search-input").val();
+    
 
 
 
 
+
+
+     // checks if user added a valid search input
+     if (!userInput) {
+        console.error("Please enter a valid search input value!")
+        return;
+    }
 
  }
- // function to display weather cards in html
+ // event listener for submit button that runs the function searchFormSubmit
+ searchFormEl.addEventListener('submit', searchFormSubmit);
 
-
- 
- 
- 
- 
- 
- 
- 
- 
- // Gets weatherAPI and data
+ // gets weatherAPI and data
  function getWeather() {
-    return fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={1a0e2919a9b4dd8457669d885e90b7b9}')
+    // return fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={1a0e2919a9b4dd8457669d885e90b7b9}')
         .then(function (response){
             return response.json();
         })
@@ -45,3 +54,5 @@
         })
     
  };
+
+ // function to display weather cards in html
