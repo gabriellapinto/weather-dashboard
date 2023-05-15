e1dda2da743bc2f4b30f378276a08b3d8ce82af9
Gabby <gabbympinto@gmail.com>
2023-05-15 14:24:51 -0400

    wrote return current weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,24 @@
-const searchBtn = $("#search-btn");
-const searchForm = $("#search-form");
-const userCity = $()
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + userCity + '&units=imperial&appid=1a0e2919a9b4dd8457669d885e90b7b9';
+const apiKey = "1a0e2919a9b4dd8457669d885e90b7b9";
+var currWeatherDiv = $("#currentWeather");
+var forecastDiv = $("#weatherForecast");
+var citiesArray;
 
-// gets weatherAPI and data
-function getWeather(requestUrl) {
-    return fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            return data
-        })
-};
+function returnCurrentWeather(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+  
+    $.get(queryURL).then(function(response){
 
-
-
-
-function showWeather() {
-    // clears data first so there is no overlapping of content
-    let fiveDayForecast = $("#five-day-forecast");
-    fiveDayForecast.empty();
-
-    let currentDate = new Date();
-    let weatherDay = $('<div>').attr("id", "weatherDay").addClass('col');
-    let weatherIcon = 'https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png';
-
-    for (let i=0; i < weatherData.list.length; i++) {
-       let weatherDate = new Date(weatherData.list[i].dt * 1000) 
-
-       let date = $("<h4>").attr("id", "dateText");
-       date.text("${weatherDate.getMonth() + 1}/${weatherDate.getDate()}/$(weatherDate.getFullYear()}");
-       weatherDay.append(date);
-
-
-
-
-
-    }
-
-
-
-
-}
-
-
-
-
-
-
-
-    // event listener for search button
-    searchBtn.click(function (event) {
-        event.preventDefault();
-        var userCity = searchForm.val();
-        // checks user input is valid
-        if (!userCity) {
-            console.error("Please enter a valid search input value!");
-            return;
-        }
-        getWeather(userCity);
+        let currTime = new Date(response.dt*1000);
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+  
+        currWeatherDiv.html(`
+        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+        <p>Temperature: ${response.main.temp} &#176;C</p>
+        <p>Humidity: ${response.main.humidity}%</p>
+        <p>Wind Speed: ${response.wind.speed} m/s</p>
+        `, returnUVIndex(response.coord))
     });
-
+  };
 
 
 
